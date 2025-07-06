ac0a2be44368aa701d2e40530d11653f3302f66b
Rtmt92 <rayantoumert.rt@gmail.com>
2025-07-06 18:34:05 +0200

    securité

--- backend/core/Database.php
+++ backend/core/Database.php
@@ -4,22 +4,30 @@ namespace Core;
 use PDO;
 use PDOException;
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
 class Database {
     private static ?PDO $instance = null;
 
-    // Configuration centralisée
-    private static string $host     = '127.0.0.1';
-    private static string $dbname   = 'dejavu';
-    private static string $username = 'root';
-    private static string $password = 'admin';
-    private static string $charset  = 'utf8mb4';
-
     public static function getConnection(): PDO {
         if (self::$instance === null) {
-            $dsn = "mysql:host=" . self::$host . ";dbname=" . self::$dbname . ";charset=" . self::$charset;
+            // Charger .env
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+            $dotenv->load();
+
+            // Variables d'environnement
+            $host     = $_ENV['DB_HOST']     ?? '127.0.0.1';
+            $dbname   = $_ENV['DB_DATABASE'] ?? 'dejavu';
+            $username = $_ENV['DB_USERNAME'] ?? 'root';
+            $password = $_ENV['DB_PASSWORD'] ?? 'admin';
+            $charset  = 'utf8mb4';
+
+            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 
             try {
-                self::$instance = new PDO($dsn, self::$username, self::$password, [
+                self::$instance = new PDO($dsn, $username, $password, [
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                 ]);
--- backend/vendor/composer/autoload_psr4.php
+++ backend/vendor/composer/autoload_psr4.php
@@ -7,10 +7,16 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Tests\\' => array($baseDir . '/tests'),
+    'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Stripe\\' => array($vendorDir . '/stripe/stripe-php/lib'),
     'Src\\Models\\' => array($baseDir . '/src/Models'),
     'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'PhpOption\\' => array($vendorDir . '/phpoption/phpoption/src/PhpOption'),
+    'GrahamCampbell\\ResultType\\' => array($vendorDir . '/graham-campbell/result-type/src'),
     'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
+    'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
     'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Core\\' => array($baseDir . '/core'),
     'Controllers\\' => array($baseDir . '/controllers'),
